66ecd4f65165b54e7552c6c49fec7e521038d31e
Edit-Mr <Edit-Mr@users.noreply.github.com>
2024-01-15 06:10:13 +0000

    Merge branch 'main' of https://github.com/EM-Tec/EM-Tec.github.io 71b7465c2ede11ecd7437a129521f19ffff47af4

--- en/js/bundle.12d2fdc41039e289953f0542aa73ecb16abde53507fe49847d4a827263c16d1cf429611aa4a2bbe54b8fe9adc74db09623418f34c55fb94756695fbb43fc8582.js
+++ en/js/bundle.12d2fdc41039e289953f0542aa73ecb16abde53507fe49847d4a827263c16d1cf429611aa4a2bbe54b8fe9adc74db09623418f34c55fb94756695fbb43fc8582.js
@@ -10,7 +10,7 @@ const imageAltClass = 'img_alt';
 
 
 const defaultSiteLanguage = 'zh-tw';
-const baseURL = 'https://EM-Tec.github.io/';
+const baseURL = 'https://emtech.cc/';
 const searchFieldClass = '.search_field';
 const searchClass = '.search';
 const goBackClass = 'button_back';
